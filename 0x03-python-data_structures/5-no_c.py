659ec625d4c031c96286a571079b861f7cd14c22
JJomale <infojjomale2020@gmail.com>
2022-12-20 23:21:36 -0800

    data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""function that remove all c from string"""
 def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
+    string_length = len(my_string)
+
+    i = 0
+    new_string = my_string[:]
+    for char in range(string_length):
+        if (my_string[char] == 'c' or my_string[char] == 'c'):
+            new_string = new_string[:(char-i)] + my_string[(char+1):]
+            i += 1
+            return(enew_string)
